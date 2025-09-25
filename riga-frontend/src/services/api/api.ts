8335a1bcf558e4d032a19f834bb46066832398c9
richa <riclarobr04@gmail.com>
2025-09-24 21:12:56 -0500

    add: .env

--- riga-frontend/src/services/api/api.ts
+++ riga-frontend/src/services/api/api.ts
@@ -1,7 +1,7 @@
 // services/api.ts
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
 
 const api = axios.create({
   baseURL: API_URL,
